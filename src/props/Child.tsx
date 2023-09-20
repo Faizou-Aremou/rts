58eb2f25e008a13b3d50deaa846107ffe4b025fe
Faïzou Aremou <faizouaremou@gmail.com>
2023-09-20 13:58:51 +0200

    React.FC

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,7 +1,20 @@
+
 export interface ChildProps {
   color: string;
+  children?: React.ReactNode;
+  onClick:()=> void;
 }
 
-export const Child = ({ color }: ChildProps) => {
-  return <div> {color} </div>;
+export const Child = ({ color, onClick, children }: ChildProps) => {
+  return <div> {color} 
+  {children}
+  <button onClick={onClick}></button>
+  </div>;
+};
+
+export const ChildFC: React.FC<ChildProps> = ({ color, onClick, children }: ChildProps) => {
+  return <div> {color} 
+  {children}
+   <button onClick={onClick}></button>
+  </div>;
 };
