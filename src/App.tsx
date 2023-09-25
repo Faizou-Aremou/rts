c49729e425f2e0f8d59505da7cd8fae4614c66a7
Faïzou Aremou <faizouaremou@gmail.com>
2023-09-25 14:14:27 +0200

    on search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import GuestList from './state/GestList';
+import UserSearch from './state/UserSearch';
 
 const App = () => {
   return (
     <div>
      <GuestList></GuestList>
+     <UserSearch/>
     </div>
   )
 }
