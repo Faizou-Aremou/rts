8ffe7827a675710f2a099a50a591cadb3f218a87
Faïzou Aremou <faizouaremou@gmail.com>
2023-09-27 14:06:59 +0200

    javascript event

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -1,12 +1,19 @@
-const EventComponent: React.FC = () => {
+import { ChangeEvent, DragEvent } from 'react';
 
-  const onChange = (e:any) => {
-    console.log(e);
-  }
+const EventComponent: React.FC = () => {
+  const onChange = (event: ChangeEvent) => {
+    console.log(event);
+  };
+  const onDragStart = (event: DragEvent) => {
+    console.log(event);
+  };
 
   return (
     <div>
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me!
+      </div>
     </div>
   );
 };
